8b97c4b545e6dae817539579d05fcc2e5e654fc5
Sophia Nara <rosu.corb@gmail.com>
2022-08-07 03:47:51 +0900

    remove unused files

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Hello } from './Hello';
-import { Info } from './Info';
+import { PrefSelect } from './PrefSelect';
 
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
-    <Hello />
-    <Info />
+    <PrefSelect />
   </div>
 );
